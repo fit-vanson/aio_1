c1c2f0ed4d548cefcd568a7667bb5d8c06ddfaf6
Administrator <sonvt.vietmmo@gmail.com>
2022-09-06 13:43:24 +0700

    123

---
+++ app/Models/ProjectHasLang.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectHasLang extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+      'project_id',
+      'lang_id',
+      'content',
+      'logo',
+      'banner',
+      'preview',
+      'video',
+      'user_design',
+      'status',
+    ];
+
+
+
+    public function project()
+    {
+        return $this->belongsTo(ProjectModel::class, 'project_id','projectid');
+    }
+
+    public function lang()
+    {
+        return $this->belongsTo(Language::class, 'lang_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_design', 'id');
+    }
+
+}
