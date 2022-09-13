4c57be6a4d2258f14213c875b1554b27ddcc422b
Administrator <sonvt.vietmmo@gmail.com>
2022-09-13 15:15:47 +0700

    update

--- resources/views/design_content/index.blade.php
+++ resources/views/design_content/index.blade.php
@@ -60,7 +60,7 @@
 
     <div class="row">
 
-        <div class="col-lg-2 col-md-2">
+        <div class="col-lg-1 col-md-1">
             <div class="card">
                 <div class="card-body">
 {{--                    <p class="card-text">--}}
@@ -84,7 +84,7 @@
         <!-- end col -->
 
 
-        <div class="col-lg-10">
+        <div class="col-lg-11">
             <div class="card">
                 <div class="card-body " id="project_detail" style="display: none" >
 
